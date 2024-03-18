af61ae264131b6a3578bc4df5bd1a190d072ac91
Vu Tran <vuth@youmed.vn>
2024-03-18 17:28:23 +0700

    test commit

--- WeatherApiService/src/main/java/com/skyapi/weatherforecast/WeatherApiServiceApplication.java
+++ WeatherApiService/src/main/java/com/skyapi/weatherforecast/WeatherApiServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherApiServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("test fork");
         SpringApplication.run(WeatherApiServiceApplication.class, args);
     }
 
